1451a68be72c4f911d3b6f882eec5b87e5d9c5ee
Anthony Becker <anthonybecker9@gmail.com>
2022-11-09 22:05:57 -0800

    woot test

--- train_rave.py
+++ train_rave.py
@@ -17,7 +17,7 @@ import GPUtil as gpu
 from udls.transforms import Compose, RandomApply, Dequantize, RandomCrop
 
 if __name__ == "__main__":
-
+    print("woot!")
     class args(Config):
         groups = ["small", "large"]
 
